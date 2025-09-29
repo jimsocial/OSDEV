aaed7dd6874ae1f46a1ff556db25457bb42cd512
jimsocial <ethan.hammond.17@gmail.com>
2025-09-29 02:05:57 -0500

    initial cargo

---
+++ test_os/target/debug/deps/test_os-04f5a0cbb0c8395f.d
@@ -0,0 +1,5 @@
+/home/ethan/my-os/test_os/target/debug/deps/test_os-04f5a0cbb0c8395f.d: src/main.rs
+
+/home/ethan/my-os/test_os/target/debug/deps/libtest_os-04f5a0cbb0c8395f.rmeta: src/main.rs
+
+src/main.rs:
