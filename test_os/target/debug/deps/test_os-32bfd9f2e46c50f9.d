56d8e321b74aa64b5058ecf16e68705728f8f5ed
Ethan Hammond <73313769+jimsocial@users.noreply.github.com>
2025-10-01 16:03:10 -0500

    Delete test_os directory

--- test_os/target/debug/deps/test_os-32bfd9f2e46c50f9.d
+++
@@ -1,5 +0,0 @@
-/home/ethan/my-os/test_os/target/debug/deps/test_os-32bfd9f2e46c50f9.d: src/main.rs
-
-/home/ethan/my-os/test_os/target/debug/deps/libtest_os-32bfd9f2e46c50f9.rmeta: src/main.rs
-
-src/main.rs:
