e9e32627a2950f5beb3d40803598e15a4076d24b
Ethan Hammond <73313769+jimsocial@users.noreply.github.com>
2025-10-01 16:15:33 -0500

    Adding test_os back after adding .gitignore

---
+++ test_os/target/debug/deps/test_os-bb6c22e0a93f7d4f.d
@@ -0,0 +1,5 @@
+/home/ethan/my-os/test_os/target/debug/deps/test_os-bb6c22e0a93f7d4f.d: src/main.rs
+
+/home/ethan/my-os/test_os/target/debug/deps/test_os-bb6c22e0a93f7d4f: src/main.rs
+
+src/main.rs:
