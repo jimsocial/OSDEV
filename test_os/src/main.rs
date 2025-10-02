ae2924a2542adf6a3d5f7b3e7f57bc9ee49ec49d
jimsocial <ethan.hammond.17@gmail.com>
2025-10-01 20:52:47 -0500

    uploaded new config file and target json file

--- test_os/src/main.rs
+++ test_os/src/main.rs
@@ -3,15 +3,15 @@
 
 use core::panic::PanicInfo;
 
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[unsafe(no_mangle)] 
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    loop {}
-}
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
